b47585ba25eef24ff35162bfb7cd0bb1eb26a56e
Emmanuel Lagarrigue <elagarrigue@gmail.com>
2020-02-10 10:45:38 -0700

    Add renderHtml

--- src/main/kotlin/refactoring/TheaterCompany.kt
+++ src/main/kotlin/refactoring/TheaterCompany.kt
@@ -2,8 +2,6 @@ package refactoring
 
 import org.joda.money.CurrencyUnit
 import org.joda.money.Money
-import kotlin.math.floor
-import kotlin.math.max
 
 data class Performance(val playID: String, val audience: Int)
 
@@ -18,7 +16,7 @@ enum class PlayType { TRAGEDY, COMEDY }
 
 data class Play(val name: String, val type: PlayType)
 
-data class StatementData(val costumer: String, val performances: List<PerformanceWithPlay>) {
+data class StatementData(val customer: String, val performances: List<PerformanceWithPlay>) {
     var totalAmount: Int = 0
     var totalVolumeCredits: Int = 0
 }
@@ -28,9 +26,11 @@ class TheaterCompany(plays: Map<String, Play>) {
     private val statementBuilder = StatementBuilder(plays)
 
     fun statement(invoice: Invoice) = renderPlainText(statementBuilder.createStatementData(invoice))
-    
+
+    fun htmlStatement (invoice: Invoice) = renderHtml(statementBuilder.createStatementData(invoice))
+
     private fun renderPlainText(data: StatementData): String {
-        var result = "Statement for ${data.costumer}\n"
+        var result = "Statement for ${data.customer}\n"
 
         data.performances.forEach { perf ->
             // print line for this order
@@ -41,6 +41,21 @@ class TheaterCompany(plays: Map<String, Play>) {
         result += "Amount owed is ${usd(data.totalAmount.toDouble())}\n"
         result += "You earned ${data.totalVolumeCredits} credits\n"
 
+        return result
+    }
+
+    private fun renderHtml(data: StatementData): String {
+        var result = "<h1>Statement for ${data.customer}</h1>\n"
+        result += "<table>\n"
+        result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>"
+
+        data.performances.forEach { perf ->
+            result += " <tr><td>${perf.play.name}</td><td>${perf.audience}</td>"
+            result += "<td>${usd(perf.amount.toDouble())}</td></tr>\n"
+        }
+        result += "</table>\n"
+        result += "<p>Amount owed is <em>${usd(data.totalAmount.toDouble())}</em></p>\n"
+        result += "<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n"
         return result
     }
 
--- src/test/kotlin/refactoring/TheaterCompanyTest.kt
+++ src/test/kotlin/refactoring/TheaterCompanyTest.kt
@@ -35,4 +35,22 @@ class TheaterCompanyTest {
 
         result `should be equal to` expected
     }
+
+    @Test
+    fun `should build statement html string`() {
+        val company = TheaterCompany(plays)
+
+        val result = company.htmlStatement(invoice)
+
+        val expected = "<h1>Statement for BigCo</h1>\n" +
+                "<table>\n" +
+                "<tr><th>play</th><th>seats</th><th>cost</th></tr> <tr><td>Hamlet</td><td>55</td><td>USD 650.00</td></tr>\n" +
+                " <tr><td>As You Like It</td><td>35</td><td>USD 580.00</td></tr>\n" +
+                " <tr><td>Othello</td><td>40</td><td>USD 500.00</td></tr>\n" +
+                "</table>\n" +
+                "<p>Amount owed is <em>USD 1730.00</em></p>\n" +
+                "<p>You earned <em>47</em> credits</p>\n"
+
+        result `should be equal to` expected
+    }
 }
